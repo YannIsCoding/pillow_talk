6594f0bf9d7256be012303f021120a5d868dbe57
Yann Parent <yann.parent4@gmail.com>
2019-08-19 17:27:46 +0200

    added route, controller and view. need To had the html to loop through pillows

--- app/controllers/pillows_controller.rb
+++ app/controllers/pillows_controller.rb
@@ -1,2 +1,7 @@
 class PillowsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+def index
+  @pillows = Pillow.all
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pillow.create(name: "chunky bacon twist")
+Pillow.create(name: "butt pillow")
+Pillow.create(name: "Super pillow")
+Pillow.create(name: "Shitty pillow")
+Pillow.create(name: "Blabla pillow")
