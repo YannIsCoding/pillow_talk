b5d98b081ac879c60b5f711166bdf51348979b8a
Yann Parent <yann.parent4@gmail.com>
2019-08-19 16:39:49 +0200

    added pillow controller

---
+++ app/controllers/pillows_controller.rb
@@ -0,0 +1,2 @@
+class PillowsController < ApplicationController
+end
---
+++ test/controllers/pillows_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PillowsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
