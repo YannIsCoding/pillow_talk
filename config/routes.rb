ffaf4de75475cd6e370e3d553be87ac5edec166d
Angelina Diener <angelinadiener@gmx.de>
2019-08-20 17:19:51 +0200

    simple show html and css + background image globally added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :pillows, only: [:index, :new, :show, :create]
+  resources :pillows, only: [:index, :new, :show, :create, :update, :edit, :destroy]
 
 end
