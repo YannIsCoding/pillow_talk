ca7055942d977987c0412b60434e81b7a135b37c
Yann Parent <yann.parent4@gmail.com>
2019-08-21 12:24:27 +0200

    basic booking fonction

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,25 @@
 class BookingsController < ApplicationController
 
   def show
-
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @pillow = Pillow.find(params[:pillow_id])
     @booking = Booking.new
   end
 
   def create
-
+    user = current_user
+    pillow = Pillow.find(params[:pillow_id])
     @booking = Booking.new(booking_params)
-    raise
-    @booking.save
-
+    @booking.user = user
+    @booking.pillow = pillow
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -31,6 +37,6 @@ class BookingsController < ApplicationController
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :pillow_id, :user_id)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
